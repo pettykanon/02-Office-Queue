efd7ec543213a60fe5b05acd8258b55ca50ef377
Lorenzo Cuccu s330260 <s330260@studenti.polito.it>
2024-10-10 01:14:27 +0200

    Get ticket UI first page

--- office-queue-client/src/GetTicket.jsx
+++ office-queue-client/src/GetTicket.jsx
@@ -1,19 +1,81 @@
-import { Container} from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
+import { Button, Container} from 'react-bootstrap';
+import { Navbar, Row,Col,Modal} from 'react-bootstrap';
+import { useState } from 'react';
 
-function ChooseService(){
-    return(<>
-    <Navbar className='bg-d'data-bs-theme='light'>
-        <Container>
-            <Navbar.Text >
-                CHOOSE A SERVICE 
-            </Navbar.Text>  
-        </Container>
-    </Navbar>
-    <Container fluid>
-        
-    </Container>
-    </>)
+function ChooseService() {
+    const services = ["servizio 1", "servizio 2", "servizio 3", "servizio 4"];
+
+    const [selectedService, setSelectedService] = useState(); 
+    const [showModal, setShowModal] = useState(false);
+
+    const handleServiceClick = (s) =>{
+        setSelectedService(s)
+        setShowModal(true)
+    }
+
+    const handleClose = () => {
+        setShowModal(false);
+        setSelectedService(null);
+    };
+
+    const handleConfirm = () => {
+        setShowModal(false);
+        setSelectedService(null);
+    };
+    
+    return (
+        <>
+            <Navbar className='bg-d custom-navbar'>
+                <Container>
+                    <Navbar.Text className='navbar-text-custom c-w'>
+                        CHOOSE A SERVICE
+                    </Navbar.Text>  
+                </Container>
+            </Navbar>
+            <Container fluid>
+                <div className="service-container">
+                    {services.map((s) => (
+                        <ServiceCard s={s} key={s} handleServiceClick={handleServiceClick}/>
+                    ))}
+                </div>
+            </Container>
+
+            <Modal show={showModal} onHide={handleClose} centered>
+                <Modal.Body className='btn-font-1 bg-d text-center py-5'>
+                    <Modal.Title className='btn-font-4'>You chose:</Modal.Title>
+                    <p>{selectedService}</p>
+                    <Row>
+                        <Col>
+                            <Button className='btn-font-3 custom-btn-canc' onClick={handleClose}>
+                                Cancel
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button className='btn-font-2 custom-btn-confirm' onClick={handleConfirm}>
+                                Get the Ticket
+                            </Button>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                
+            </Modal>
+        </>
+    );
+}
+
+function ServiceCard(props) {
+    return (
+        <div className="service-card">
+            <Button className="service-button" onClick={()=>props.handleServiceClick(props.s)}>
+                <Row>
+                    <Col className='btn-font-1'>{props.s}</Col>
+                </Row>
+                <Row>
+                    <Col className='btn-font-2'>Questo è un servizio</Col>
+                </Row>
+            </Button>
+        </div>
+    );
 }
 
 export {ChooseService}
