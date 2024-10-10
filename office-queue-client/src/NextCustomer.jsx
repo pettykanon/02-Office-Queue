a6864b11f62e47e4f48913221d3891dca258f9c2
FRANCESCO CHIAIA s322348 <s322348@studenti.polito.it>
2024-10-10 11:15:47 +0200

    NextCustomer bug fix

--- office-queue-client/src/NextCustomer.jsx
+++ office-queue-client/src/NextCustomer.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import './NextCustomer.css';
 
 function NextCustomer() {
   return (<>
@@ -13,7 +14,7 @@ function NextCustomer() {
     <Container fluid className='mt-4'>
       <Row>
         <Col md={5}>
-          <div className='rect-left bg-d'>
+          <div className='rect-left bg-d mx-5 my-5'>
             <h3 className='rect-title'>Services provided:</h3>
             <ol className="list-custom">
               <li>Shipping</li>
@@ -36,7 +37,7 @@ function NextCustomer() {
           </div>
         </Col>
         <Col md={7}>
-            <div className="rect-right bg-green">
+            <div className="rect-right bg-green mx-5 my-5">
               <p className="serving-text">Serving Client:</p>
               <p className='serving-client'>B18</p>
             </div>
