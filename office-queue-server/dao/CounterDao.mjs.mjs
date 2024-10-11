938b41663a7e113f3055c2c94758131047cec857
mick <miperseo@gmail.com>
2024-10-11 09:37:17 +0200

    add CounterDao default function

--- office-queue-server/dao/CounterDao.mjs.mjs
+++ office-queue-server/dao/CounterDao.mjs.mjs
@@ -1,14 +1,19 @@
 import db from "../db.mjs";
 
 
-// retrieve all the services offered by a counter
-export default function getServices(counterId) {
-    return new Promise((resolve,Object) => {
+export default function Counterdao() {
+
+
+    // retrieve all the services offered by a counter
+    this.getCounterServices = (counterId) => {
         
-        const query = "SELECT * FROM `counter` WHERE `counterId` = ?";
-        db.all(query, [counterId], (error, rows) => {
-            if (error) throw error;
-            resolve(rows);
+        return new Promise((resolve,Object) => {
+            
+            const query = "SELECT * FROM `counter` WHERE `counterId` = ?";
+            db.all(query, [counterId], (error, rows) => {
+                if (error) throw error;
+                resolve(rows);
+            })
         })
-    })
+    }
 }
