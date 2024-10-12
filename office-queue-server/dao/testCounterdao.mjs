af09f225f2f05ae6ed4d3981bb328dfc606abe20
mick <miperseo@gmail.com>
2024-10-12 16:14:31 +0100

    add test ticket done

--- office-queue-server/dao/testCounterdao.mjs
+++ office-queue-server/dao/testCounterdao.mjs
@@ -1,10 +1,9 @@
 
 //this is the test file for creating mock data to test the CounterDao.mjs file
 import Counterdao  from './CounterDao.mjs';
+import TicketDao from './ticketDao.js';
 
 const CounterDao = new Counterdao();
-
-
 
 
 async function getServices() {
@@ -17,6 +16,22 @@ async function getServices() {
         console.error('Error getting services:', error);
   }
 }
-
 getServices();
 
+async function setAsDone() {
+  const ticketId = 2;
+  let ticket = await TicketDao.getTicketById(ticketId);
+  console.log('Ticket:', ticket);
+  
+  try {
+    const result = await CounterDao.setTicketDone(ticketId);
+  } 
+  catch (error) {
+    console.error('Error setting as done:', error);
+  }
+  
+  ticket = await TicketDao.getTicketById(ticketId);
+  console.log('Ticket:', ticket);
+}
+
+setAsDone();
