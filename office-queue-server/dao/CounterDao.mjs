702246dfd12630386fa8f4c0b22257840077010e
mick <miperseo@gmail.com>
2024-10-12 16:52:50 +0100

    add get next ticket code from service queue

--- office-queue-server/dao/CounterDao.mjs
+++ office-queue-server/dao/CounterDao.mjs
@@ -6,9 +6,9 @@ export default function Counterdao() {
 
     /* retrieve all the services offered by a counter*/
     this.getCounterServices = (counterId) => {
-        
-        return new Promise((resolve,Object) => {
-            
+
+        return new Promise((resolve, Object) => {
+
             const query = 'SELECT serviceid FROM daily_setting WHERE counterId = ?';
             db.all(query, [counterId], (error, rows) => {
                 if (error) throw error;
@@ -18,10 +18,22 @@ export default function Counterdao() {
     }
 
     /* sets the status of the previous custoemr's ticket to done*/
-    this.setTicketDone = (ticketId) => {
-        return new Promise((resolve,Object) => {
-            const query = 'UPDATE ticket SET statusId = 3 WHERE id = ?';
-            db.run(query, [ticketId], (error, rows) => {
+    this.setTicketDone = (ticketCode) => {
+        return new Promise((resolve, Object) => {
+            const query = 'UPDATE ticket SET statusId = 3 WHERE code = ?';
+            db.run(query, [ticketCode], (error, rows) => {
+                if (error) throw error;
+                resolve(rows);
+            })
+        })
+    }
+
+    //gets the next ticket in a queue of a service
+    this.getServiceWaitingTicket = (serviceID) => {
+        return new Promise((resolve, Object) => {
+
+            const query = 'SELECT code FROM ticket WHERE serviceId = ? AND statusId = 1';
+            db.get(query, [serviceID], (error, rows) => {
                 if (error) throw error;
                 resolve(rows);
             })
