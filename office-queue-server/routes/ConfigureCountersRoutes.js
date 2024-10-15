3841ca2e711f3e64e1abcadfe50e791680ad9441
mick <miperseo@gmail.com>
2024-10-16 00:02:22 +0200

    finish post counter configurations

--- office-queue-server/routes/ConfigureCountersRoutes.js
+++ office-queue-server/routes/ConfigureCountersRoutes.js
@@ -10,38 +10,59 @@ import express from 'express';
 const router = express.Router();
 const CounterDao = new Counterdao();
 
-router.get('/api/setCounters', async (req, res) => {
-
+router.get('/:counterId', async (req, res) => {
+    const {counterId} = req.params;
+    
     try {
-        const allServices = await CounterDao.getCounterServices();
-        return res.status(200).json(allServices);
+        const counterServices = await CounterDao.getCounterServices(counterId);
+        const counterservicesIDs = counterServices.map(service => service.serviceId);
+       
+        return res.status(200).json(counterservicesIDs);
     } catch (error) {
         console.error('Error fetching services:', error.message);
         return res.status(500).json({ error: 'Internal server error' });
     }
 })
 
-router.post('/api/settingcounters/:counterId',
+router.post('/:counterId',
     [
         body("services").exists().withMessage("services are required"),
     ]    
     ,async (req, res) => {
         const {counterId} = req.params;
         const { services } = req.body;
+        console.log(services)
         
         //get current counter's services
-        const currentCounterServicesIds = await CounterDao.getCounterServices(counterId);
+        const currentCounterServices = await CounterDao.getCounterServices(counterId);
+        console.log(currentCounterServices);
+        
+        const currentCounterServicesIds = currentCounterServices.map(service => service.serviceId);
+        console.log(currentCounterServicesIds);
 
         try {
             for (const service of services) {
-                const servicerecord = await ServiceDao.getServiceByName(service);
                 //check if counter already has that service
-
+                if (currentCounterServicesIds.includes(service)) {
+                    console.log("Service already exists in counter: " + service);
+                    const index = currentCounterServicesIds.indexOf(service);
+                    currentCounterServicesIds.splice(index, 1);
+                    console.log(currentCounterServicesIds);
+                    continue;
+                }
                 
-                //set the counter using this service
-                await CounterDao.setService(counterId, servicerecord.id);
+                //add the service to counter
+                console.log("Adding service to counter: " + service);
+                await CounterDao.setService(counterId, service);
             }
-
+            for (const id of currentCounterServicesIds) {
+                //remove the service not used anymore from counter
+                try {
+                    await CounterDao.removeService(counterId, id);
+                } catch (error) {
+                    console.error('Error removing service:', error.message);
+                }
+            }
             return res.status(200).json({ message: 'Services set successfully' });
         } catch (error) {
             console.error('Error setting services:', error.message);
